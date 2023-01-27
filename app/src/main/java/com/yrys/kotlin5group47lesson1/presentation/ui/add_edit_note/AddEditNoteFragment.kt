f7d6d3c1f8a6b537f363dcf8544c5318594fcfdd
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2023-01-27 20:51:52 +0600

    dfdjdf

--- app/src/main/java/com/yrys/kotlin5group47lesson1/presentation/ui/add_edit_note/AddEditNoteFragment.kt
+++ app/src/main/java/com/yrys/kotlin5group47lesson1/presentation/ui/add_edit_note/AddEditNoteFragment.kt
@@ -15,8 +15,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class AddEditNoteFragment :
     BaseFragment<FragmentAddEditNoteBinding, AddEditNoteViewModel>(R.layout.fragment_add_edit_note) {
 
-    private val b = 1
-    private val a = 0
+    private val j = 1
+    private val a = 20
     override val binding by viewBinding(FragmentAddEditNoteBinding::bind)
     override val viewModel by viewModels<AddEditNoteViewModel>()
     private var note: Note? = null
